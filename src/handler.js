175f7148f58834d62b026ca5d1c4e70d30394325
Shannon Jensen <shannonjensen@gmail.com>
2017-11-23 17:12:15 +0000

    modify map to reset without problems

--- src/handler.js
+++ src/handler.js
@@ -6,17 +6,21 @@ const path = require('path');
 const cleanData = require('./logic');
 const querystring = require('querystring');
 
-// const api_key = 'D1aKjY2zznvOfRideoCk8IGQfO2gTBct';
-const api_key = process.env.API_KEY;
+const api_key = 'D1aKjY2zznvOfRideoCk8IGQfO2gTBct';
+//const api_key = process.env.API_KEY;
 
 const homeHandler = (request, response) => {
   const filePath = path.join(__dirname, '..', 'public', 'index.html')
-  fs.readFile(filePath, function(err, file) {
-    if(err){
-      response.writeHead(500, {'Content-Type':'text/plain'});
+  fs.readFile(filePath, function (err, file) {
+    if (err) {
+      response.writeHead(500, {
+        'Content-Type': 'text/plain'
+      });
       response.end('Server error');
     }
-    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
     response.end(file);
   })
 }
@@ -30,45 +34,52 @@ const staticFileHandler = (request, response, endpoint) => {
   }
   const extension = endpoint.split('.')[1];
   const filePath = path.join(__dirname, '..', endpoint)
-  fs.readFile(filePath, function(err, file) {
-    if(err && err.code === 'ENOENT'){
-      response.writeHead(404, {'Content-Type':'text/plain'});
+  fs.readFile(filePath, function (err, file) {
+    if (err && err.code === 'ENOENT') {
+      response.writeHead(404, {
+        'Content-Type': 'text/plain'
+      });
       response.end('404 Not Found');
-    }
-    else if(err){
-      response.writeHead(500, {'Content-Type':'text/plain'});
+    } else if (err) {
+      response.writeHead(500, {
+        'Content-Type': 'text/plain'
+      });
       response.end('Server error');
+    } else {
+      response.writeHead(200, 'Content-Type: ' + extensionType[extension]);
+      response.end(file);
     }
-    else {
-    response.writeHead(200, 'Content-Type: ' + extensionType[extension]);
-    response.end(file);
-  }
   })
 }
 
 const searchHandler = (req, response, endpoint) => {
   var queries = querystring.parse(endpoint.split("?")[1]);
   let buildUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + api_key + "&latlong=" + queries.ll + "&radius=" + queries.radius + "&unit=miles&sort=date,asc";
-  if (queries.sdate){
+  if (queries.sdate) {
     buildUrl += "&startDateTime=" + queries.sdate + "T00:00:00Z";
   }
-  if (queries.edate){
-      buildUrl += "&endDateTime=" + queries.edate + "T23:59:59Z";
-    }
+  if (queries.edate) {
+    buildUrl += "&endDateTime=" + queries.edate + "T23:59:59Z";
+  }
   const options = {
     url: buildUrl,
     method: 'GET'
-  }
-
+  };
   request(options, (err, res, body) => {
-    if(err){
+    if (err) {
       console.log("error :", error);
     }
     var outcome = JSON.parse(body);
     var newOutcome = cleanData(outcome);
-    response.writeHead(200, {"Content-Type": "text/html"});
+    response.writeHead(200, {
+      "Content-Type": "text/html"
+    });
     response.end(JSON.stringify(newOutcome));
   });
 }
 
-module.exports = {homeHandler, staticFileHandler, searchHandler};
+module.exports = {
+  homeHandler,
+  staticFileHandler,
+  searchHandler
+};
